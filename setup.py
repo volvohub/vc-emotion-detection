8f2a8b594a058811d503e13ade67d1502cb5b363
SKulgod <skulgod@yahoo.com>
2025-07-22 15:39:12 -0400

    Add cokkie cutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='an ml project to dtect emotions in text',
+    author='skg',
+    license='',
+)
